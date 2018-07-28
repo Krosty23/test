eda8faba034b2667d13f9d663c5d956b9d97d461
krosty23 <jorge_o.murillo@hotmail.com>
2018-07-28 01:01:55 -0500

    repositorio

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const os = require('os');
-console.log(os.platform());
-console.log(os.release());
+const http = require('http');
+
+const colors = require('colors');
+
+const handleserver = function (req, res){
+    res.writeHead(200, { 'content-type': 'text/html' });
+res.write('<h1> Hola mundo</h1>');
+res.end();
+}
+
+const server = http.createServer(handleserver);
+
+server.listen(3000, function(){
+    console.log('server on port 3000'.yellow)
+});
