1ff371517a9389ca8ae4af8d9c154fd529fc287c
krosty23 <jorge_o.murillo@hotmail.com>
2018-07-27 23:27:59 -0500

    repositorio

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const matematicas = require ("./matematicas.js");
 
-console.log(matematicas.add(1, 2))
+console.log(math);
+/*console.log(matematicas.add(1, 2))
 console.log(matematicas.math(1, 2))
 console.log(matematicas.multiply(1, 2))
 console.log(matematicas.divide(1, 2))
-console.log(matematicas.divide(1, 0))
+console.log(matematicas.divide(1, 0))*/
